9060bcfb54fd893510dcb4453e74e9f2caa89b4e
Arizona mango Juice <codingandmorecoding@gmail.com>
2025-10-22 23:36:07 -0600

    Fix search detection logic

--- api/test.js
+++ api/test.js
@@ -11,7 +11,7 @@ export default function handler(req, res) {
     const { tags = '', page = '1', limit = '42', mode = 'unified' } = req.query;
 
     // If this is a search request, handle it as search
-    if (tags || req.url.includes('search')) {
+    if (tags && tags.length > 0) {
         try {
             console.log(`Search via test endpoint: tags="${tags}", page=${page}, mode=${mode}`);
 
